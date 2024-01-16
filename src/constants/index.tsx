80faccb9e6a78ea5492c1d5683a48414d39a037d
Daniel Soto <dasg.1993@gmail.com>
2024-01-16 11:47:34 -0500

    Add URLS_API constant and create custom hook for Fetch data

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -2,3 +2,13 @@ export enum ROUTES {
   HOME = 'Home',
   DETAILS = 'Details',
 }
+
+export const URLS_API = {
+  base: 'https://api.artic.edu/api',
+  version: 'v1',
+  artworks: 'artworks',
+  paginate: {
+    page: 1,
+    limit: 10,
+  },
+};
