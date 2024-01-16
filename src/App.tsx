d66002e3b05b24a936d8e3f2d9aff8da7fdffcad
Daniel Soto <dasg.1993@gmail.com>
2024-01-16 10:32:12 -0500

    Update file paths, add simulator option for iOS , create folder structure screens

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import Home from './screens/home';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Home />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
---
+++ src/screens/home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function Home(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}
+
+export default Home;
